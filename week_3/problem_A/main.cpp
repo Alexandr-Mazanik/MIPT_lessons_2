67452b82fba31cf821f1f2ae19f5700d6e8a64ef
Alexandr <alexandrmazanik@gmail.com>
2021-02-20 00:55:09 +0300

    w3_pr_A

---
+++ week_3/problem_A/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int count;
+	cin >> count;
+
+	float average = 0;
+	int *array = new int[count];
+	for (int i=0; i < count; i++)
+	{
+		cin >> array[i];
+		average += array[i];
+	}
+	average = average / count;
+	
+	int sum = 0;
+	for (int i=0; i < count; i++)
+		if (array[i] > average)
+			sum += array[i];
+
+	cout << endl << sum << endl;
+
+	return 0;
+}
