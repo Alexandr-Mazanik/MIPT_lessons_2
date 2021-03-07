19047e06cc0d2b295a30c530b8883e480fce85a3
Alexandr <alexandrmazanik@gmail.com>
2021-03-07 12:56:25 +0300

    w3

---
+++ week_3/problem_H/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct Cat {
+    char *name;
+    unsigned int id;
+    double weight, length;
+    unsigned int mice_caught;
+};
+
+Cat* get_home_for_a_cats_pride(unsigned int n)
+{
+	//Cat *cats = new Cat [n];
+
+	for (int i=0; i<n; i++)
+	{
+		Cat.name * name = new 
+	}
+
+	return cats;
+}
+
+void clear_home_of_a_cats_pride(Cat *cats, unsigned int n)
+{
+	delete [] cats;
+}
+
+int main() {
+    unsigned int n;
+    cin >> n;
+    Cat *a = get_home_for_a_cats_pride(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].name >> a[i].weight >> a[i].length >> a[i].mice_caught;
+        a[i].id = i;
+    }
+    for (int i = 0; i < n; i++)
+        cout << a[i].name << " ";
+    cout << endl;
+    clear_home_of_a_cats_pride(a, n);
+    return 0;
+}
